af52d2456b84edef4b54d0084f3464b27a731bf5
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-17 00:29:30 -0400

    Adds responsiveness
    Big error happening with setIndex

--- slides/src/components/slides.js
+++ slides/src/components/slides.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 export default function Slide({ films }) {
   const [index, setIndex] = useState(0);
@@ -9,22 +11,95 @@ export default function Slide({ films }) {
     <>
       <Title title={film.title} />
       <Card film={film} />
-      <ButtonBar film={film} setIndex={setIndex} />
+      <ButtonBar maxIndex = {films.length - 1} index={index} setIndex={setIndex} />
     </>
   );
 }
 
-function Title({ title }) {}
-function Card({ film }) {}
+function Title({ title }) {
+  return <h4 className="title">{title}</h4>;
+}
+function Card({ film }) {
+  return (
+    <>
+      <div className="card mb-3">
+        <div className="row g-0">
+          <Image src = {film.image}/>
+        
+          <div className="col-md-7">
+            <Body film={film} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
-function Image({ src }) {}
+function Image({ src }) {
+  return (
+    <div className="col-md-5 img-container">
+      <img src={src} alt="Movie Cover" className="img-fluid rounded-start" />
+    </div>
+  )
+}
 
-function Body({ film }) {}
+function Body({ film }) {
+  return (
+    <div className="card-body">
+      <h5 className="card-title">{film.original_title}</h5>
+      <p className="card-text"><small className="text-muted">{film.release_date}</small></p>
+      <p className="card-text">{film.description}</p>
+    </div>
+  )
+}
 
-function ButtonBar({ film }, { setIndex }) {}
+function ButtonBar({ maxIndex }, { index }, { setIndex }) {
+  return (
+    <div className="button-ba d-flex justify-content-center">
+      <StartOverButton setIndex={setIndex} />
+      <BackButton maxIndex = { maxIndex } index ={index} setIndex={setIndex} />
+      <NextButton maxIndex = { maxIndex } index = {index} setIndex = {setIndex} />
+    </div>
+  )
+}
 
-function StartOverButton({ film }, { setIndex }) {}
+function StartOverButton({ setIndex }) {
+  return (
+    <button type="button" className="mx-2 btn btn-primary" onClick={() => setIndex(0)}>
+      <FontAwesomeIcon icon={solid('arrow-rotate-left')} /> Start Over
+    </button>
+  )
+}
 
-function BackButton({ film }, { setIndex }) {}
+function BackButton({ maxIndex }, { index }, { setIndex }) {
+  let handleBack = () => {
+    index -= 1;
+    if (index < 0) {
+      index = maxIndex
+    }
+    
+    return setIndex( index )
+  }
 
-function NextButton({ film }, { setIndex }) {}
+  return (
+    <button type="button" className="mx-2 btn btn-primary" onClick={handleBack()}>
+      <FontAwesomeIcon icon={solid('caret-left')} /> Back
+    </button>
+  )
+}
+
+function NextButton({ maxIndex }, { index }, { setIndex }) {
+  let handleNext = () => {
+    index += 1;
+    if (index > maxIndex) {
+      index = 0
+    }
+    
+    return setIndex( index )
+  }
+  return (
+    <button type="button" className="mx-2 btn btn-primary">
+      Next <FontAwesomeIcon icon={solid('caret-right')} onClick={handleNext()} />
+    </button>
+  )
+}
